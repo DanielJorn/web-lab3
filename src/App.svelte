189a227a76726de44678102155d57cf45e09ace3
Anton Proshchenko <0954264@gmail.com>
2021-12-10 15:33:43 +0200

    add checker

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,71 @@
 <script>
-	import http from './helpers/RequestHelper';
-	import OperationDocsHelper from './helpers/OperationDocsHelper';
-	import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
-	import { setClient,subscribe } from "svelte-apollo";
-	import {WebSocketLink} from '@apollo/client/link/ws'
+  import http from "./helpers/RequestHelper";
+  import OperationDocsHelper from "./helpers/OperationDocsHelper";
+  import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+  import { setClient, subscribe } from "svelte-apollo";
+  import { WebSocketLink } from "@apollo/client/link/ws";
 
+  function createApolloClient() {
+    const wsLink = new WebSocketLink({
+      uri: "wss://web-laba-3.herokuapp.com/v1/graphql",
+      options: {
+        reconnect: true,
+      },
+    });
+    const cache = new InMemoryCache();
+    return new ApolloClient({
+      link: wsLink,
+      cache,
+    });
+  }
+  const client = createApolloClient();
+  setClient(client);
+  const subscribed = subscribe(OperationDocsHelper.SubscriptionAllPokemon());
+  const addPokemon = async () => {
+    const name = prompt("Enter name: ") || "";
+    const ability = prompt("Enter ability: ") || "";
+    await http.startExecuteMyMutation(
+      OperationDocsHelper.addOnePokemon(name, ability),
+    );
+    // pokemons.update(n=>[...n,{name,ability}])
+  };
 
-	function createApolloClient() {
-	const wsLink = new WebSocketLink({
-		uri:"wss://web-laba-3.herokuapp.com/v1/graphql",
-		options:{
-			reconnect:true
-		}
-	})
-	const cache = new InMemoryCache();
-	const client = new ApolloClient({
-		link: wsLink,
-		cache,
-	});
-	return client;
- 	}
-	const client = createApolloClient();
- 	setClient(client);
-	 const subscribed = subscribe(OperationDocsHelper.SubscriptionAllPokemon())
-	const addPokemon = async () => {
-		const name = prompt("Enter name: ") || "";
-		const ability = prompt("Enter ability: ") || "";
-		await http.startExecuteMyMutation(OperationDocsHelper.addOnePokemon(name,ability));
-		// pokemons.update(n=>[...n,{name,ability}])
-	}
-
-	const deletePokemon = async () => {
-		const name = prompt("Enter name to delete: ")|| "";
-		await http.startExecuteMyMutation(OperationDocsHelper.deletePokemonsByName(name));
-		// pokemons.update(n=>n.filter(item => item.name!==name));
-	}
+  const deletePokemon = async () => {
+    const name = prompt("Enter name to delete: ") || "";
+    await http.startExecuteMyMutation(
+      OperationDocsHelper.deletePokemonsByName(name),
+    );
+    // pokemons.update(n=>n.filter(item => item.name!==name));
+  };
 </script>
 
 <main>
-	{#if $subscribed.loading}
-	<h1>Loading</h1>
-	{:else if $subscribed.error}
-	<h1>Error</h1>
-	{:else}
-	<button on:click={addPokemon}>Add pokemon</button>
-		<button on:click={deletePokemon}>Delete pokemon</button>
-		<table border="1">
-			<thead>
-				<th>name</th>
-				<th>ability</th>
-			</thead>
-			<tbody>
-				{#each $subscribed.data.pokemons as pokemon}
-					<tr>
-						<th>{pokemon.name}</th>
-						<th>{pokemon.ability}</th>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
-{/if}
-	
+  {#if $subscribed.loading}
+    <h1>Loading</h1>
+  {:else if $subscribed.error}
+    <h1>Error</h1>
+  {:else}
+    <button on:click={addPokemon}>Add pokemon</button>
+    <button on:click={deletePokemon}>Delete pokemon</button>
+    <table border="1">
+      <thead>
+        <th>name</th>
+        <th>ability</th>
+      </thead>
+      <tbody>
+        {#each $subscribed.data.pokemons as pokemon}
+          <tr>
+            <th>{pokemon.name}</th>
+            <th>{pokemon.ability}</th>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  {/if}
 </main>
 
 <style>
+  main {
+    margin: 0;
+  }
 </style>
--- src/helpers/OperationDocsHelper.js
+++ src/helpers/OperationDocsHelper.js
@@ -1,35 +1,43 @@
 import { gql } from "@apollo/client";
 
 export default class OperationDocsHelper {
-  static getAllPokemons = () => `
-  query MyQuery {
-        pokemons {
-          name
-          id
-          ability
-        }
-      }`;
-  static addOnePokemon = (name, ability) => `
-  mutation MyMutation {
-    insert_pokemons_one(object: {ability: "${ability}", name: "${name}"}) {
-      ability
-      name
-    }
+  static getAllPokemons() {
+    return `
+    query MyQuery {
+          pokemons {
+            name
+            id
+            ability
+          }
+        }`;
   }
-  `;
-  static deletePokemonsByName = (name) => `
-  mutation MyMutation {
-    delete_pokemons(where: {name: {_eq: "${name}"}}) {
-      affected_rows
-    }
-  }
-  `;
-  static SubscriptionAllPokemon = () => gql`
-    subscription MySubscription {
-      pokemons {
-        name
+  static addOnePokemon(name, ability) {
+    return `
+    mutation MyMutation {
+      insert_pokemons_one(object: {ability: "${ability}", name: "${name}"}) {
         ability
+        name
       }
     }
-  `;
+    `;
+  }
+  static deletePokemonsByName(name) {
+    return `
+    mutation MyMutation {
+      delete_pokemons(where: {name: {_eq: "${name}"}}) {
+        affected_rows
+      }
+    }
+    `;
+  }
+  static SubscriptionAllPokemon() {
+    return gql`
+      subscription MySubscription {
+        pokemons {
+          name
+          ability
+        }
+      }
+    `;
+  }
 }
